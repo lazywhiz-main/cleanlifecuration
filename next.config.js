b327df0ecafda438d9ff24b6d011810ce3ef876f
Mina Baek <minabaek@MinadotDMacBook-Air.local>
2025-04-10 18:25:31 +0900

    Update next.config.js with required settings

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'standalone',
+  images: {
+    domains: ['images.unsplash.com'],
+  },
+  // 開発環境でのクロスオリジンリクエストを許可
+  async headers() {
+    return [
+      {
+        source: '/_next/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+        ],
+      },
+    ]
+  }
 };
 
 module.exports = nextConfig; 
